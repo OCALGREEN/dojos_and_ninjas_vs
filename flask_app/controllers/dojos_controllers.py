bfd88d1db315a82c301fb5e9c270c9ec5c0999c3
Oger Carrillo <ogercarrillo@gmail.com>
2022-01-23 20:55:12 -0800

    display_and_create_new_dojos

--- flask_app/controllers/dojos_controllers.py
+++ flask_app/controllers/dojos_controllers.py
@@ -5,7 +5,8 @@ from flask_app.models.dojo import Dojo
 # Home Page
 @app.route("/")
 def home():
-    return render_template("dojo.html")
+    list_of_dojos = Dojo.get_all() # gets all the dojos and puts it in the variable
+    return render_template("dojo.html", all_dojos = list_of_dojos) # creates a variable ready for html
 
 @app.route("/update_dojo", methods=["POST"])
 def update_dojo():
